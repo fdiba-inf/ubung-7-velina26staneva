449f7dd5cd6597b34fd09b939e7e24e5326f527b
velina26staneva <velin4eto01@gmail.com>
2020-11-12 17:45:50 +0000

    1

--- src/exercise7/NumberCounter.java
+++ src/exercise7/NumberCounter.java
@@ -19,12 +19,22 @@ public class NumberCounter {
         System.out.print("Search for number: ");
         int number = input.nextInt();
 
-        int numberOccurrences = 0; // Call method
+        int numberOccurrences = 0; 
+        numberOccurrences = countNumberOccurrences(number, numbers);
         System.out.println("Number occurrences: " + numberOccurrences);
     }
 
     public static int countNumberOccurrences(int number, int[] numbers) {
-        // Implement method
-        return 0;
+      int counter = 0;
+      for ( int index = 0; index < numbers.length; index++) {
+          if (number == numbers[index]) {
+            counter++;
+          
+      
+        }
+      }
+      
+        
+        return counter;
     }
 }
